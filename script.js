3e689904c1de33eadeae59a87baa8841bf952da5
Jacobsin12 <tristanjacob755@gmail.com>
2026-03-01 19:04:19 -0600

    Modificando Script.js

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 5, // 5 usuarios simulados
-  duration: '10s', // prueba cortita de 10 segundos
+  vus: 10,
+  duration: '30s',
 };
 
 export default function () {
-  // 1. Probamos la ruta principal (raiz)
-  // CAMBIA "tu-app.render.com" por la URL donde esté subida tu app XD
-  let res1 = http.get('https://devops-jacobsin.onrender.com/'); 
+  // Usamos la API de prueba oficial de k6
+  let res1 = http.get('https://test.k6.io'); 
   check(res1, { 'status es 200': (r) => r.status === 200 });
 
   sleep(1);
 
-  // 2. Probamos otra ruta (ejemplo: la de usuarios o index)
-  let res2 = http.get('https://devops-jacobsin.onrender.com/users');
+  let res2 = http.get('https://test.k6.io/contacts.php');
   check(res2, { 'status es 200': (r) => r.status === 200 });
 
   sleep(1);
